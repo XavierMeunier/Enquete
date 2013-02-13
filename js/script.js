357056b1a002047a66267e9d3927511e63f4a1c1
Nod <ganondorf_00@hotmail.fr>
2013-02-13 15:36:55 +0100

    Adaptation de la nav

--- js/script.js
+++ js/script.js
@@ -15,42 +15,43 @@ $(function() {
 
 	h = $(window).height();
 	w = $(window).width();
-	myZone = 'zone3';
+	myZone = 'home';
 
 	$('#container').css({'width': Math.round(w*2), 'height': Math.round(h*2)});
 	$('#container section').css({'width': Math.round(w), 'height': Math.round(h)});
-	$('#container section#zone3').css({'margin-left': Math.round(w/2)});
+	$('#container section#home').css({'margin-left': Math.round(w/2)});
 	$('#container').css({marginLeft:Math.round(-w/2)+'px', marginTop:Math.round(-h)+'px'});
 	$(window).resize(function(){ 
 	    redimensionnement();
 	}); 
 
-	$('#container section').on('click', function(){
+	$('nav div a').on('click', function(e){
+		e.preventDefault();
 		$this = $(this);
-		zone = $this.attr('id');
+		destination = $this.attr('href').substr(1);
+		console.log(destination);
+		$('nav div').removeClass();
+		$this.parent().addClass('active');
 		redimensionnement();
 
-		if(zone == 'zone1') {
-			if($('.redband1').attr('width')==0){
+		if(destination == 'home') {
+			
+			/*if($('.redband1').attr('width')==0){
 				$('.redband1').delay(500).animate({ width:'0'+'%'},500,function(){
-					//$('.redband2').animate({ width:'50'+'px'},500,function(){
-					$('#container').animate({ marginLeft:Math.round(-w/2)+'px', marginTop:Math.round(-h)+'px'});
-					myZone = 'zone3';		
-				});
-			}else {
-				$('#container').animate({ marginLeft:Math.round(-w/2)+'px', marginTop:Math.round(-h)+'px'});
-				myZone = 'zone3';
-			}
+					//$('.redband2').animate({ width:'50'+'px'},500,function(){		
+				});*/
+			$('#container').animate({ marginLeft:Math.round(-w/2)+'px', marginTop:Math.round(-h)+'px'});
+			myZone = 'home';
 		}
-		else if(zone == 'zone3') {
+		else if(destination == 'plus-enquetes') {
 			$('#container').animate({ marginTop:'0px', marginLeft:Math.round(-w)+'px' });
-			myZone = 'zone2';
+			myZone = 'plus-enquetes';
 		}
-		else if(zone == 'zone2') {
-			$('#container').animate({ marginLeft:'0px' });
-			myZone = 'zone1';
+		else if(destination == 'equipe') {
+			$('#container').animate({ marginLeft:'0px', marginTop:'0px' });
+			myZone = 'equipe';
 		}
-		return zone;
+		return destination;
 	});
 });
 
@@ -64,15 +65,15 @@ function redimensionnement() {
 	}
 	$('#container').css({'width': Math.round(w*2), 'height': Math.round(h*2)});
 	$('#container section').css({'width': Math.round(w), 'height': Math.round(h)});
-	$('#container section#zone3').css({'margin-left': Math.round(w/2)});
+	$('#container section#home').css({'margin-left': Math.round(w/2)});
 
-	if(myZone == 'zone1') {
+	if(myZone == 'equipe') {
 		$('#container').css({ marginLeft:'0px' });
 	}
-	else if(myZone == 'zone3') {
+	else if(myZone == 'home') {
 		$('#container').css({marginTop:Math.round(-h)+'px', marginLeft:Math.round(-w/2)+'px'});
 	}
-	else if(myZone == 'zone2') {
+	else if(myZone == 'plus-enquetes') {
 		$('#container').css({marginLeft:Math.round(-w)+'px'});
 	}
 }
